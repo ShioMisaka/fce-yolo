631095d04bbf6b1d6201bd9c18dd4f967bcb6c26
ShioMisaka <94329511+ShioMisaka@users.noreply.github.com>
2025-12-31 14:28:13 +0800

    feat:添加了测试训练的方法

---
+++ train_test.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# 1. 加载模型
+model_path = "yolo11n-bifpn.yaml"
+model = YOLO(model_path)
+
+model.train(
+    data="/home/shiomisaka/workplace/ai-playground/datasets/MY_TEST_DATA/data.yaml",
+    epochs=2,
+    imgsz=640,
+    batch=2,
+    device="cpu"
+)
